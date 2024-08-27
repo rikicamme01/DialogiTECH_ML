52654ec45ed9df3ebd3c2451f18a266b7459eacf
riccardo <283824@studenti.unimore.it>
2024-08-27 19:29:31 +0200

    commit

--- src/scripts/bert_seg_train.py
+++ src/scripts/bert_seg_train.py
@@ -14,6 +14,7 @@ run = neptune.init_run(
     api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI5NTkzYjg4Yy1jMTVhLTQ2NDktYmFjOC0yNzZmZDEyMDFlOTcifQ==",
 ) 
 
+#%%
 from transformers import AutoTokenizer
 
 from utils.utils import plot_loss, seed_everything
@@ -21,16 +22,18 @@ from loggers.neptune_logger import NeptuneLogger
 from trainers.bert_seg_trainer import BertSegTrainer
 from datasetss.ie_hyperion_dataset import IEHyperionDataset, train_val_split
 
-if len(sys.argv) != 2:
+'''if len(sys.argv) != 2:
     print("ERROR:  config_file path not provided")
-    sys.exit(1)
+    sys.exit(1)'''
 
 # Repository paths
 # './' local
 # './RepML/ cluster
 
+script_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 try: 
-    with open (sys.argv[1] + 'config/bert_seg_train.yml', 'r') as file:
+    with open (script_dir + '/config/bert_seg_train.yml', 'r') as file:
         config = yaml.safe_load(file)        
 except Exception as e:
     print('Error reading the config file')
@@ -39,12 +42,9 @@ print('config file loaded!')
 
 seed_everything(config['seed'])
 
-logger = NeptuneLogger()
-logger.run['config'] = config
-
-df = pd.read_csv(sys.argv[1] + 'data/segmenter/ie_hyperion_train.csv', converters={'Stralci': literal_eval, 'Repertori': literal_eval})
-df_s2 = pd.read_csv(sys.argv[1] + 'data/segmenter/ie_s2_hyperion_train.csv', converters={'Stralci': literal_eval, 'Repertori': literal_eval})
-test_df = pd.read_csv(sys.argv[1] + 'data/segmenter/ie_s2_hyperion_test.csv', converters={'Stralci': literal_eval, 'Repertori': literal_eval})
+df = pd.read_excel(script_dir + '/data/segmenter/dataset_finale_train.xlsx', converters={'Stralci': literal_eval, 'Repertori': literal_eval})
+df_s2 = pd.read_csv(script_dir + '/data/segmenter/ie_s2_hyperion_train.csv', converters={'Stralci': literal_eval, 'Repertori': literal_eval})
+test_df = pd.read_csv(script_dir + '/data/segmenter/ie_s2_hyperion_test.csv', converters={'Stralci': literal_eval, 'Repertori': literal_eval})
 
 model_name = config['model']
 #model_name = 'MiBo/SegBert'
@@ -57,6 +57,7 @@ train_dataset, _ = train_val_split(df, model_name)
 _, val_dataset = train_val_split(df_s2, model_name)
 test_dataset = IEHyperionDataset(test_df, model_name)
 
+#%%
 trainer = BertSegTrainer()
 
 history = trainer.fit(model,
@@ -67,24 +68,26 @@ history = trainer.fit(model,
             config['n_epochs'],
             torch.nn.CrossEntropyLoss(weight = torch.Tensor(config['loss_weights'])))
 
-logger.run['history'] = history
+run['history'] = history
 fig = plot_loss(history['train_loss'], history['val_loss'])
-logger.run["loss_plot"].upload(neptune.types.File.as_image(fig))
+run["loss_plot"].upload(neptune.types.File.as_image(fig))
 
 out = trainer.test(model, val_dataset, config['batch_size'], torch.nn.CrossEntropyLoss(weight = torch.Tensor(config['loss_weights'])))
-logger.run['val/norm_metrics'] = out['normalized_metrics']
-logger.run['val/metrics'] = out['metrics']
-logger.run['val/loss'] = out['loss']
-logger.run['val/predicted_spans'] = out['predicted_spans']
+run['val/norm_metrics'] = out['normalized_metrics']
+run['val/metrics'] = out['metrics']
+run['val/loss'] = out['loss']
+run['val/predicted_spans'] = out['predicted_spans']
 
 
 out = trainer.test(model, test_dataset, config['batch_size'], torch.nn.CrossEntropyLoss(weight = torch.Tensor(config['loss_weights'])))
-logger.run['test/norm_metrics'] = out['normalized_metrics']
-logger.run['test/metrics'] = out['metrics']
-logger.run['test/loss'] = out['loss']
-logger.run['test/predicted_spans'] = out['predicted_spans']
+run['test/norm_metrics'] = out['normalized_metrics']
+run['test/metrics'] = out['metrics']
+run['test/loss'] = out['loss']
+run['test/predicted_spans'] = out['predicted_spans']
 
 hf_token = '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'
 if config['save']:
     model.push_to_hub("SegBert", use_temp_dir=True, use_auth_token=hf_token)
     train_dataset.tokenizer.push_to_hub("SegBert", use_temp_dir=True, use_auth_token=hf_token)
+
+# %%
